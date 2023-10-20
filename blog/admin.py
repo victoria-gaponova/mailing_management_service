0bae0a56717ffbdafb9f06a590d272855b0f7fd2
Vika <victoria.gaponava@gmail.com>
2023-10-20 23:46:55 +0300

    add admin for blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import BlogPost
+
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = "pk", "title", "owner"
