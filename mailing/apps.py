019894ae0c58a6e4de1f6f7ad9d083b8832561ed
Vika <victoria.gaponava@gmail.com>
2023-10-21 00:18:48 +0300

    start task manager

--- mailing/apps.py
+++ mailing/apps.py
@@ -1,6 +1,14 @@
+from time import sleep
+
 from django.apps import AppConfig
 
 
 class MailingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mailing'
+
+    def ready(self):
+        from jobs import updater
+        sleep(2)
+        updater.start()
+
